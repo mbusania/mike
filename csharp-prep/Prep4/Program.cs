638cfb0e8f94ea6ee9e5db474c68d5826fb87d89
mbusania <145633082+mbusania@users.noreply.github.com>
2024-03-03 12:39:37 +0800

    sz

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+namespace Prep4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            // Ask the user for a series of numbers until they enter 0
+            int number;
+            do
+            {
+                Console.Write("Enter number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number != 0)
+                    numbers.Add(number);
+            } while (number != 0);
+
+            // Compute the sum of the numbers
+            int sum = numbers.Sum();
+
+            // Compute the average of the numbers
+            double average = numbers.Average();
+
+            
+            int max = numbers.Max();
+
+            
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
     }
 }
