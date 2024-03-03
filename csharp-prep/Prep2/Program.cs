4cfec80eb310adbe7cb443b195e5ec8376aad38f
mbusania <145633082+mbusania@users.noreply.github.com>
2024-03-03 12:36:42 +0800

    yessir

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+namespace Prep2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            // Ask the user for their grade percentage
+            Console.Write("Enter your grade percentage: ");
+            int gradePercentage = Convert.ToInt32(Console.ReadLine());
+
+            // Determine the letter grade
+            char letter;
+
+            if (gradePercentage >= 90)
+                letter = 'A';
+            else if (gradePercentage >= 80)
+                letter = 'B';
+            else if (gradePercentage >= 70)
+                letter = 'C';
+            else if (gradePercentage >= 60)
+                letter = 'D';
+            else
+                letter = 'F';
+
+            // Display the letter grade
+            Console.WriteLine($"Your letter grade is: {letter}");
+
+            // Check if the user passed the course
+            if (letter != 'F')
+                Console.WriteLine("Congratulations! You passed the course.");
+            else
+                Console.WriteLine("Don't worry, you can try again next time.");
+        }
     }
 }
