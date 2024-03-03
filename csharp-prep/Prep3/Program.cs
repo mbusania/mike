28e4043d488b4d7d8ad7bc2f78c24e64b0e810d5
mbusania <145633082+mbusania@users.noreply.github.com>
2024-03-03 12:38:42 +0800

    yep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
+namespace Prep3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            // Generate a random magic number between 1 and 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            // Play the Guess My Number game
+            int guess;
+            do
+            {
+                // Ask the user for their guess
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                // Check if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                    Console.WriteLine("Higher");
+                else if (guess > magicNumber)
+                    Console.WriteLine("Lower");
+                else
+                    Console.WriteLine("You guessed it!");
+            } while (guess != magicNumber);
+        }
     }
 }
